e7095ddcfcdc57c225f21cb1f09239ebc6a57f0a
Stefan R. <ruppe@protonmail.com>
2024-05-13 10:48:29 +0000

    initial commit with basic function structure

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# our card symbols
+heart = '\u2665'
+star = '\u2605'
+music = '\u266b'
+sun = '\u2600'
+cloud = '\u2601'
+umbrella = '\u2602'
+soccer_ball = '\u26bd'
+basketball = '\U0001F3C0'
+
+def initialize_board():
+    pass
+def draw_board(board, card1, card2):
+    pass
+def pick_card(player):
+    pass
+def display_scores():
+    pass
+def display_final_score():
+    pass
+
+def main():
+    username = input('Please tell me your name: \n')
+    print(f'\nHello {username}! \u2665 Welcome to this little memory game.')
+    # print(heart, star, music, sun, cloud, umbrella, soccer_ball, basketball)
+    print('-' * 40 + '\n')
+
+main()
