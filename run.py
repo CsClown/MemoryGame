7991924a1a3d140214ba31b532079ab807ef4523
Stefan R. <ruppe@protonmail.com>
2024-05-14 09:48:26 +0000

    add display of chosen cards, check for same card chosen, add draw revealed cards

--- run.py
+++ run.py
@@ -13,11 +13,15 @@ joker = '\U0001F0CF'
 
 row_labels = ['A','B','C','D']
 
+
 class player:
     def __init__(self, name):
         self.name = name
         self.score = 0
         self.pairs = []
+        self.pick1 = []
+        self.pick2 = []
+        self.active = True
 
     def add_score(self):
         self.score += 1
@@ -34,11 +38,17 @@ class player:
 # class pick:
 #     def __init__(self, card1, card2)
 
+# class game_board:
+#     def __init__(self, size)
+#         self.size = size
+#         self.board = []
+
+#     def ini
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def initialize_board():
+def initialize_board(size = 4):
     board = [
         [heart, heart, star, star],
         [music, music, sun, sun],
@@ -47,8 +57,9 @@ def initialize_board():
     ]
     return board
 
+board = initialize_board()
 
-def draw_board(board, card1 = None, card2 = None):
+def draw_board(board, pick1 = None, pick2 = None):
     """
     Draw the board depending on game state
     with either one, two or no card revealed
@@ -60,7 +71,9 @@ def draw_board(board, card1 = None, card2 = None):
         print(f'\n\n{x}    ', end = '')
         
         for y in range(len(board[0])):
-            if card1 and card1 == board[x][y]:
+            if pick1 and pick1 == [y,x]:
+                print(board[x][y], end = '    ')
+            elif pick2 and pick2 == [y,x]:
                 print(board[x][y], end = '    ')
             else: print(joker, end = '    ')
     print('\n' + '-' * 30 )
@@ -89,13 +102,13 @@ def player_turn(player):
     """
     Facilitates a whole turn of the active player
     """
-    while True:
-        if card1:
-            if card2:
-        else
-            card1 = pick_card()        
-            card2 = pick_card
-
+    # while True:
+    #     if card1:
+    #         if card2:
+    #     else
+    #         card1 = pick_card()        
+    #         card2 = pick_card
+    pass
 
 
 def pick_card(player):
@@ -104,18 +117,38 @@ def pick_card(player):
     calls validate_input for input validation ;)
     returns list
     """
-    while True:
-        print('+++ Please pick a card! Example: A1 +++')
-        
-        pick_str = input('+++ YOUR PICK: ')
-        pick_strip = [i.strip() for i in pick_str]
-        pick_ls = [string for string in pick_strip]
-        if validate_input(pick_strip):
-            pick_val = [pick_strip[0].upper(),int(pick_strip[1])]
-            break
+    
 
-    print(f'You chose: {pick_val[0]}{pick_val[1]}')
-    return pick_strip
+    while True:
+        if player.pick1 == []:
+            print('+++ Please pick your FIRST card! Example: A1 +++')
+            
+            pick_str = input('+++ YOUR PICK: ')
+            pick_strip = [i.strip() for i in pick_str]
+            pick_ls = [string for string in pick_strip]
+            if validate_input(pick_strip):
+                player.pick1 = [pick_strip[0].upper(),int(pick_strip[1])]
+                # draw_board(board, player.pick1)
+                print(f'You chose: {player.pick1[0]}{player.pick1[1]}')
+                card1 = [ord(player.pick1[0]) - 65, player.pick1[1]]
+                draw_board(board, card1)
+        elif player.pick2 == []:
+            print('+++ Please pick your SECOND card! Example: A1 +++')
+            
+            pick_str = input('+++ YOUR PICK: ')
+            pick_strip = [i.strip() for i in pick_str]
+            pick_ls = [string for string in pick_strip]
+            if validate_input(pick_strip):
+                player.pick2 = [pick_strip[0].upper(),int(pick_strip[1])]
+                if player.pick2 == player.pick1:
+                    print('+++ You cannot chose the same card twice! +++')
+                    player.pick2 = []
+                    continue
+                print(f'You chose: {player.pick2[0]}{player.pick2[1]}')
+                card2 = [ord(player.pick2[0]) - 65, player.pick2[1]]
+                draw_board(board, card1, card2)
+                break
+    return
 
 def check_pair(card1, card2):
     if card1 == card2:
@@ -138,22 +171,21 @@ def main():
     print('-' * 40)
     username = input('Please tell me your name: \n')
     print(f'\nHello {username}! \u2665 Welcome to this little memory game.')
-    
     print('-' * 40 + '\n')
-
-
+    
     player1 = player(username)
     player2 = player('computer')
-
-    active_player = 'user'
+    player2.active = False
+    active_player = player1
     
     card1 = None
     card2 = None
 
-    board = initialize_board()
-    draw_board(board)
-    pick = pick_card(username)
-    draw_board(board, )
+    
+    
+    
+    
+    yes = pick_card(active_player)
     
     
 
