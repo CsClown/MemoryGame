2ebec09a20572aa63a1d5e450099b075609be370
Stefan R. <ruppe@protonmail.com>
2024-05-14 08:47:12 +0000

    basic formating and input error handling

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 
 # our card symbols
 heart = '\u2665'
@@ -8,6 +9,12 @@ cloud = '\u2601'
 umbrella = '\u2602'
 soccer_ball = '\u26bd'
 basketball = '\U0001F3C0'
+joker = '\U0001F0CF'
+
+row_labels = ['A','B','C','D']
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def initialize_board():
     board = [
@@ -20,46 +27,93 @@ def initialize_board():
 
 
 def draw_board(board, card1 = None, card2 = None):
-   # for col in range(len(board)) 
-    print(' '* 4 + 'A' + ' ' * 4 + 'B' + ' ' * 4 + 'C' + ' ' * 4 + 'D\n')
+    """
+    Draw the board depending on game state
+    with either one, two or no card revealed
+    """
+    clear_terminal()
+    print('\n' + '-' * 30 )
+    print(' '* 5 + 'A' + ' ' * 5 + 'B' + ' ' * 5 + 'C' + ' ' * 5 + 'D')
     for x in range(len(board)):
-        print('\n')
+        print(f'\n\n{x}    ', end = '')
+        
         for y in range(len(board[0])):
-            print(board[x][y], end = '    ')
+            if card1 and card1 == board[x][y]:
+                print(board[x][y], end = '    ')
+            else: print(joker, end = '    ')
+    print('\n' + '-' * 30 )
     print('\n')
+    
 
 def validate_input(pick):
-    return True
+    """
+    validate user input (list)
+    """
+    try: 
+        if len(pick) != 2:
+            raise ValueError(
+                f'Exactly 2 values required, you provided {len(pick)}'
+            )
+        elif pick[0].upper() not in row_labels:
+            raise ValueError(
+                f'Please pick a letter A - D, you provided "{pick[0].upper()}"'
+            )
+    except ValueError as e:
+        print('*' * 40)
+        print(f'Invalid data: {e}, please try again\n')
+        return False
+    else: return True
 
 def pick_card(player):
+    """
+    User Input for choosing a card on the board
+    calls validate_input for input validation ;)
+    returns list
+    """
     while True:
-        print('Please pick a card with a Letter and a Number')
-        print('for example: A,1 ')
-        pick_str = input('your pick: ')
-        pick_ls = pick_str.split(',')
-        if validate_input(pick_ls):
+        print('+++ Please pick a card! Example: A1 +++')
+        
+        pick_str = input('+++ YOUR PICK: ')
+        pick_strip = [i.strip() for i in pick_str]
+        pick_ls = [string for string in pick_strip]
+        if validate_input(pick_strip):
+            pick_val = [pick_strip[0].upper(),int(pick_strip[1])]
             break
-    return pick_ls 
+
+    print(f'You chose: {pick_val[0]}{pick_val[1]}')
+    return pick_strip
 
 def check_pair(card1, card2):
+    pass
+
+def remove_pair(pick):
     pass
 
 
 def display_scores():
     pass
+
 def display_final_score():
     pass
 
 def main():
+    
     print('-' * 40)
     username = input('Please tell me your name: \n')
     print(f'\nHello {username}! \u2665 Welcome to this little memory game.')
-    # print(heart, star, music, sun, cloud, umbrella, soccer_ball, basketball)
+    
     print('-' * 40 + '\n')
+
+    active_player = 'user'
+    user_pairs = []
+    computer_pairs = []
+    card1 = None
+    card2 = None
 
     board = initialize_board()
     draw_board(board)
-    print(pick_card(username))
+    pick = pick_card(username)
+    draw_board(board, )
     
     
 
